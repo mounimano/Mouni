5b536380bf40abbdaef5e90cb548e48b74157543
mounimano <mounikamanoharan2006@gmail.com>
2025-09-14 21:01:32 +0530

    Add files via upload

---
+++ method.java
@@ -0,0 +1,20 @@
+import java.util.Scanner
+public class method{
+static int add(int a,intb)
+{
+    return a+b;}
+public static void main[string[]args]{
+    Scanner kbd=new Scanner(System.in);
+    int n1=kbd.nextInt();
+    int n2=kbd.nextInt();
+    return(n1+n2);
+}
+public static void main[string[]args]{
+    System.out.println(add)));
+}
+}
+}
+
+
+}
+}
