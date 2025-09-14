a7d0a465d3d96f51778c4a7d5156cd17c04f94c9
mounimano <mounikamanoharan2006@gmail.com>
2025-09-14 20:58:58 +0530

    Add files via upload

---
+++ mano.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class mano{
+public static void main(String[]args){
+    Scanner Scanner=new Scanner(System.in);
+    int size=kbd.nextInt();
+     int arr[]=new int[size];
+     int sum=0;
+     for(int i=)
+       arr[i]=kbd.nextInt();
+       
+       
+
