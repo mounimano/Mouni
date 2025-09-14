8f04b72b3c0b6a9225f2f2d93fa1d18f333a2c0a
mounimano <mounikamanoharan2006@gmail.com>
2025-09-14 21:01:06 +0530

    Add files via upload

---
+++ jam.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+ public class jam {
+    public static void main(String[]args)
+    {
+    arr[i]=kbd.nextInt();
+    int min=arr[0];
+
+    for (int i=1;i<size;i++)
+    {
+    if (min>arr[i])
+    min=arr[i];
+    }
+    System.out.println("MIN "+MIN);
+
+    }
+    
+}
